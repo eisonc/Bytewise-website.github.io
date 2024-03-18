9b99b0648f71206d98f85038d64665f0ceb6392f
eisonc <163723588+eisonc@users.noreply.github.com>
2024-03-18 21:59:56 +0800

    reset 1

---
+++ src/pages/home/Footer.jsx
@@ -0,0 +1,84 @@
+import { Link } from "react-scroll";
+import React from "react";
+
+export default Footer;
+
+function Footer() {
+  return (
+    <footer className="footer-container">
+      <div className="footer-link-container">
+        <div>
+          <img src="./assets/nike.png" width={100} alt="512" />
+        </div>
+        <div className="footer-items">
+          <ul>
+            <li>
+              <Link
+              activeClass="navbar-active-content"
+              spy = {true}
+              smooth = {true}
+              offset = {-70}
+              duration = {500}
+              to ="heroSection"
+              className="navbar-content">
+                  Home
+              </Link>
+            </li>
+            <li>
+              <Link 
+              activeClass="navbar-active-content"
+              spy = {true}
+              smooth = {true}
+              offset = {-70}
+              duration = {500}
+              to ="about-section"
+              className="navbar-content">
+                  About Us
+              </Link>
+            </li>
+            <li>
+              <Link 
+              activeClass="navbar-active-content"
+              spy = {true}
+              smooth = {true}
+              offset = {-70}
+              duration = {500}
+              to ="courses"
+              className="navbar-content">
+                  Courses
+              </Link>
+            </li>
+            <li>
+              <Link 
+              activeClass="navbar-active-content"
+              spy = {true}
+              smooth = {true}
+              offset = {-70}
+              duration = {500}
+              to ="instructor"
+              className="navbar-content">
+                  Instructors
+              </Link>
+            </li>
+            <li>
+              <Link
+              activeClass="navbar-active-content"
+              spy = {true}
+              smooth = {true}
+              offset = {-70}
+              duration = {500}
+              to ="contactus"
+              className="navbar-content">
+                  Contact Us
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <hr className="divider" />
+      <div className="footer-content-container">
+        <p className="footer-content">Copyright X Coding Acaademy 2024</p>
+      </div>
+    </footer>
+  );
+}
---
+++ src/pages/home/Hero.jsx
@@ -0,0 +1,19 @@
+export default function HeroSection() {
+    return (
+        <section id="heroSection" className="hero-section">
+            <div className="hero-section-img">
+                <img src="./assets/nike.png" alt="nike logo" />
+            </div>
+            <div className="hero-section-content-box">
+                <div className="hero-section-content">                    
+                    <h1 className="hero-section-title">
+                        Unlock the value of coding for everyone.
+                    </h1>
+                    <p className="hero-section-description">
+                        Apply coding skills to finance, law, design, medicine and more! need to add more to this *katie pls
+                    </p>   
+                </div>
+            </div>
+        </section>
+    );
+}
