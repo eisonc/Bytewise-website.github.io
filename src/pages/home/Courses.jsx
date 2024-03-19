5e4065d1a3f3971e35fc52bb0485ecb33cd5d2de
eisonc <163723588+eisonc@users.noreply.github.com>
2024-03-20 03:24:04 +0800

    working version to deploy with new formatting

--- src/pages/home/Courses.jsx
+++ src/pages/home/Courses.jsx
@@ -9,7 +9,7 @@ export default function Courses() {
         
             <div className = "course-container">
                 <h1 className ="course-section-heading"> Available Programs </h1>
-                <p className="course-section-subheading"> Please contact us if you don't see programs which interest you. We offer tailor-made individual projects as well.
+                <p className="course-section-subheading"> Please contact us if you don't see programs which interest you. We offer tailor-made individual projects just for you.
                 </p>
             </div>
             <div className ="course-section-container">
